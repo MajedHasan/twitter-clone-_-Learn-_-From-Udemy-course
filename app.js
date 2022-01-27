3844f6839f53286c4f154771c7c60a2ba4934c97
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-01-27 17:03:48 +0600

    Template Engine 'Pug' Added - It's express js template

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3003;
 
-const server = app.listen(port, () => console.log("Server Listening on port " + port))
+const server = app.listen(port, () => console.log("Server Listening on port " + port));
+
+app.set("view engine", "pug");
+
+app.get("/", (req, res, next) => {
+    res.status(200).send("Yahoo! ");
+})
