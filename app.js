97d9b178433d46876345f83c2501f8e1690bb34f
Mehedi Hasan Majed <mdmajedhasan599@gmail.com>
2022-01-27 17:18:16 +0600

    First pug template Added

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const port = 3003;
 const server = app.listen(port, () => console.log("Server Listening on port " + port));
 
 app.set("view engine", "pug");
+app.set("views", "views");
 
 app.get("/", (req, res, next) => {
-    res.status(200).send("Yahoo! ");
+    res.status(200).render("home");
 })
